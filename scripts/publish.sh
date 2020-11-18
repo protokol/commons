199ab7733de97d4a65c829c9b14476bbdd358114
Kristjan Kosic <kristjan@protokol.com>
2020-11-18 09:11:35 +0100

    chore: remove script

--- scripts/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd packages
-cd utils && npm publish --tag beta --access public --tolerate-republish && cd ..
-cd client && npm publish --tag beta --access public --tolerate-republish && cd ..
-cd cli && npm publish --tag beta --access public --tolerate-republish && cd ..
